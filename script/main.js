5db3740c8d63c4df80c778b24b44dcc8cabd71a6
OyLekk <lekgaming0@gmail.com>
2025-03-03 01:09:49 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -193,7 +193,7 @@ const animationTimeline = () => {
         }, {
             scale: 1,
             rotationY: 0,
-            color: "#ff69b4",
+            color: "#B43757",
             ease: Expo.easeOut,
         },
         0.1,
